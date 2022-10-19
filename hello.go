2a8b08fe8153b7d3a23e3f4eefbda3e55842284f
Eduardo Oliveira <eduardooliveira256@gmail.com>
2022-10-19 16:49:46 -0300

    Casting

--- hello.go
+++ hello.go
@@ -6,12 +6,14 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"strconv"
 )
 
 var pl = fmt.Println
+var tp = reflect.TypeOf
 
 func main() {
-	dataTypes()
+	casting()
 }
 
 func nameQuestion() {
@@ -43,4 +45,32 @@ func dataTypes() {
 	pl(reflect.TypeOf(3.14))
 	pl(reflect.TypeOf(true))
 	pl(reflect.TypeOf("Hello"))
+}
+
+func casting() {
+	cV1 := 1.5
+	cV2 := int(cV1)
+
+	pl(cV2)
+	pl(tp(cV2))
+
+	cV3 := "50000000"
+	cV4, err := strconv.Atoi(cV3)
+
+	pl(cV4, err, tp(cV4))
+
+	cV5 := 50000000
+	cV6 := strconv.Itoa(cV5)
+
+	pl(cV6)
+
+	cV7 := "3.14"
+	if cV8, err := strconv.ParseFloat(cV7, 64); err == nil {
+		pl(cV8, err, tp(cV8))
+	} else {
+		log.Fatal(err)
+	}
+
+	cV9 := fmt.Sprintf("%f", 3.14)
+	pl(cV9, tp(cV9))
 }
